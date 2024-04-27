34c6e565e5bf104433b38d7af13e61af925f4cb1
Fu1ro <147928940+Fu1ro@users.noreply.github.com>
2024-04-27 16:52:51 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,29 @@
 from pygame import *
 from random import randint
  
-
+# Звуки
 mixer.init()
 mixer.music.load('space.ogg')
 mixer.music.play()
 fire_sound = mixer.Sound('fire.ogg')
- 
+# Картинки
 img_back = "galaxy.png" 
-img_hero = "rocket.png" 
-
+img_hero = "rocket.png"
+img_bullet = "bullet.png" 
+# Шрифт та тексти
 font.init()
+font1 = font.Font(None, 80)
+win = font1.render('YOU WIN!', True, (255, 255, 255))
+lose = font1.render('YOU LOSE!', True, (180, 0, 0))
 font2 = font.Font(None, 36)
 
 img_enemy = "ufo.png"
-
+# Рахунки
 score = 0
+goal = 10
+max_lost = 3
 lost = 0
-
+# Основний клас
 class GameSprite(sprite.Sprite):
 
     def __init__(self, player_image, player_x, player_y, size_x, size_y, player_speed):
@@ -35,6 +41,7 @@ class GameSprite(sprite.Sprite):
 
     def reset(self):
         window.blit(self.image, (self.rect.x, self.rect.y))
+        # Клас ворога
 class Enemy(GameSprite):
     def update(self):
         self.rect.y += self.speed
@@ -44,12 +51,16 @@ class Enemy(GameSprite):
             self.rect.y = 0
             lost = lost + 1
 
-        
-
-# клас головного гравця
+     # Клас набою   
+class Bullet(GameSprite):
+    def update(self):
+        self.rect.y += self.speed
+        if self.rect.y < 0:
+            self.kill()
+# Клас гравця
 class Player(GameSprite):
  
-    # метод для керування спрайтом стрілками клавіатури
+    # Керування спрайтом
     def update(self):
         keys = key.get_pressed()
         if keys[K_w] and self.rect.y > 5:
@@ -62,57 +73,78 @@ class Player(GameSprite):
             self.rect.x += self.speed
         
  
-    # метод "постріл" (використовуємо місце гравця, щоб створити там кулю)
+    # Постріл
     def fire(self):
-        pass
+        bullet = Bullet(img_bullet, self.rect.centerx, self.rect.top, 15, 20, -15)
+        bullets.add(bullet)
  
  
-# створюємо віконце
+# Створення вікна
 win_width = 700
 win_height = 500
 display.set_caption("Shooter")
 window = display.set_mode((win_width, win_height))
 background = transform.scale(image.load(img_back), (win_width, win_height))
  
-# створюємо спрайти
+# Створення спрайта
 ship = Player(img_hero, 300, win_height - 100, 80, 100, 20)
  
-# змінна "гра закінчилася": як тільки вона стає True, в основному циклі перестають працювати спрайти
+# Змінна "гра закінчилася": як тільки вона стає True, в основному циклі перестають працювати спрайти
 finish = False
  
 # Основний цикл гри:
-run = True  # прапорець скидається кнопкою закриття вікна
+run = True  
 
 monsters = sprite.Group()
 for i in range(1, 6):
     monster = Enemy(img_enemy, randint(80, win_width - 80), -40, 80, 50, randint(1, 5))
     monsters.add(monster)
+bullets = sprite.Group()
  
 while run:
-    # подія натискання на кнопку Закрити
+    # Кнопка "Закрити"
     for e in event.get():
         if e.type == QUIT:
             run = False
+        elif e.type == KEYDOWN:
+            if e.key == K_SPACE:
+                fire_sound.play()
+                ship.fire()
  
     if not finish:
-        # оновлюємо фон
+        # Оновлення фону
         window.blit(background, (0, 0))
 
         text = font2.render("Рахунок: " + str(score), 1, (255, 255, 255))
         window.blit(text, (10, 20))
 
         text_lose = font2.render("Пропущено: " + str(lost), 1, (255, 255, 255))
-        window.blit(text, (10, 50))
+        window.blit(text_lose, (10, 50))
  
-        # рухи спрайтів
+        # Рух спрайтів
         ship.update()
         monsters.update()
+        bullets.update()
 
  
-        # оновлюємо їх у новому місці при кожній ітерації циклу
+        # Оновлення спрайтів у новому місці
         ship.reset()
         monsters.draw(window)
+        bullets.draw(window)
+        # Програш/Перемога
+        collides = sprite.groupcollide(monsters, bullets, True, True)
+        for c in collides:
+            score = score + 1
+            monster = Enemy(img_enemy, randint(80, win_width - 80), -40, 80, 50, randint(1, 5))
+            monsters.add(monster)
+
+        if sprite.spritecollide(ship, monsters, False) or lost >= max_lost:
+            finish = True
+            window.blit(lose, (200, 200))
+        if score >= goal:
+            finish = True
+            window.blit(win, (200, 200))
  
         display.update()
-    # цикл спрацьовує кожні 0.05 секунд
+    # Цикл спрацьовує кожні 0.06 секунд
     time.delay(60)
